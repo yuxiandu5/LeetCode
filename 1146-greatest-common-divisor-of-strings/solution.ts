7bdb72ec72e095180e6feeb7845bff12f93435c9
YuxianDu <yuxiandu5@gmail.com>
2025-12-30 01:46:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56 MB (52.62%)

---
+++ 1146-greatest-common-divisor-of-strings/solution.ts
@@ -0,0 +1,15 @@
+function gcdOfStrings(str1: string, str2: string): string {
+    if(str1 + str2 !== str2 + str1) return ""
+
+    const commonDivisor = gcd(str1.length, str2.length)
+
+    return str1.substring(0, commonDivisor)
+};
+
+function gcd(a, b) {
+    if(b === 0) {
+        return a
+    }
+
+    return gcd(b, a % b)
+}
