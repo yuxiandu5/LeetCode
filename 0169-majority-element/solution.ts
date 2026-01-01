e72bc89194494be4ebcb6200830ece1483eafef9
YuxianDu <yuxiandu5@gmail.com>
2026-01-01 21:51:26 +0000

    [LeetCode Sync] Runtime - 1 ms (85.60%), Memory - 58.4 MB (57.45%)

---
+++ 0169-majority-element/solution.ts
@@ -0,0 +1,28 @@
+// Output
+// 1. return number
+
+// Approach
+// 1. Boyer–Moore Voting Algorithm.
+// 2. declare a count and current.
+// 3. scan left to right
+// 4. if num === current + 1 else - 1
+// 5. therefore if count === 0 meaning num is going to be the new current
+// 6. at the end current will be the number with most appeared times
+
+function majorityElement(nums: number[]): number {
+    let count = 0
+    let current = nums[0]
+
+    for(const num of nums) {
+        if(count === 0) {
+            current = num
+        }
+        if(num === current) {
+            count++
+        } else {
+            count--
+        }
+    }
+
+    return current
+};
