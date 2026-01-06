b8dc954e5e097da1bdf40d91b440b88a3a994ed7
YuxianDu <yuxiandu5@gmail.com>
2026-01-06 00:09:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56.1 MB (62.00%)

---
+++ 0520-detect-capital/solution.ts
@@ -0,0 +1,15 @@
+function detectCapitalUse(word: string): boolean {
+    if(word === word.toUpperCase()) return true
+    if(word === word.toLowerCase()) return true
+
+    const firstLetter = word[0]
+    const rest = word.slice(1)
+    if (
+        firstLetter === firstLetter.toUpperCase() &&
+        rest === rest.toLowerCase()
+    ) {
+        return true;
+    }
+
+    return false
+};
