1668915699d87cbfe71e8c940da154b0897d898e
YuxianDu <yuxiandu5@gmail.com>
2026-01-06 23:31:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56.3 MB (78.94%)

--- 0001-two-sum/solution.ts
+++ 0001-two-sum/solution.ts
@@ -1,13 +1,21 @@
+// return array of indicies
+
+// 1. scan left to right for loop
+// 3. for every num, calculate the diff and try to look for the diff in the record
+// 2. recrod each num with its index with a Record
+// 4. if match return cur index and record index
+
 function twoSum(nums: number[], target: number): number[] {
     const record: Record<number, number> = {}
-        
-    for(let i = 0; i < nums.length; i++) {
-        const diff = target - nums[i]
 
-        if(record[diff] !== undefined){
-            return [record[diff], i]
+    for(let i = 0; i < nums.length; i++) {
+        const cur = nums[i]
+        const diff = target - cur
+
+        if(record[diff] !== undefined) {
+            return [i, record[diff]]
         }
 
-        record[nums[i]] = i
+        record[cur] = i  
     }
 };
