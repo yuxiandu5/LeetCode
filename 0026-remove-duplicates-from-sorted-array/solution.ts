d9c55fbcf04ed2eeb65620ccf9346da3c5ba8122
YuxianDu <yuxiandu5@gmail.com>
2026-01-25 08:08:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 58.8 MB (58.48%)

--- 0026-remove-duplicates-from-sorted-array/solution.ts
+++ 0026-remove-duplicates-from-sorted-array/solution.ts
@@ -1,17 +1,22 @@
-// mutate nums
-// return number
+// 1. mutate nums
+// 2. return number
 
 // 1. const write = 0
-// 3. for loop
-// 4. if cur !== prev
-// 5. nums[write] = num write++
-// 6. return write
+// 2. for loop 
+// 3. if prev !== cur nums[write] = cur write++ 
+// 4. return write
+
+// [0,0,1,1,1,2,2,3,3,4]
 
 function removeDuplicates(nums: number[]): number {
+    if(nums.length === 1) {
+        return 1
+    }
+
     let write = 1
 
     for(let i = 1; i < nums.length; i++) {
-        if(nums[i] !== nums[i - 1]){
+        if(nums[i] !== nums[i - 1]) {
             nums[write] = nums[i]
             write++
         }
