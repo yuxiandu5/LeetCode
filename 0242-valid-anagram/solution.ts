b06fdb87a36df7f2c673607c3fbbd3081af8c1b2
YuxianDu <yuxiandu5@gmail.com>
2026-02-01 19:40:25 +0000

    [LeetCode Sync] Runtime - 6 ms (90.97%), Memory - 56.6 MB (92.87%)

--- 0242-valid-anagram/solution.ts
+++ 0242-valid-anagram/solution.ts
@@ -1,12 +1,7 @@
-// Output
-// 1. return boolean
-
-// Approach
-// 1. first check for length
-// 2. for string1 we write it into a hashmap with key = letter value = count
-// 3. for string2 we increment or decrement based on hashmap
-// 4. if any count goes to negative return 
-// 5. and since length is the same there wont be any 0, 0, 1, 0
+// 1. check if length are equal
+// 2. loop through s add each letter with count inao a hashmap
+// 3. loop through t if any of the value < 0 return false
+// 4. return true
 
 function isAnagram(s: string, t: string): boolean {
     if(s.length !== t.length) {
@@ -15,21 +10,13 @@ function isAnagram(s: string, t: string): boolean {
 
     const record: Record<string, number> = {}
 
-    for(const letter of s) {
-        if(!record[letter]) {
-            record[letter] = 0
-        }
+    for(let i = 0;i < s.length;i++) {
+        const charS = s[i]
+        const charT = t[i]
 
-        record[letter] += 1
+        record[charS] = (record[charS] ?? 0) + 1
+        record[charT] = (record[charT] ?? 0) - 1
     }
 
-    for(const letter of t) {
-        if(!record[letter]) {
-            return false
-        }
-
-        record[letter] -= 1
-    }
-
-    return true
+    return Object.values(record).every(char => char === 0)
 };
