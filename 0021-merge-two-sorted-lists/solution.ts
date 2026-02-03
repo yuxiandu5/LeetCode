d20235dcd1c74ef9989728890bbd1e88c04a1e63
YuxianDu <yuxiandu5@gmail.com>
2026-02-03 18:57:35 +0000

    [LeetCode Sync] Runtime - 1 ms (44.23%), Memory - 58.5 MB (56.22%)

---
+++ 0021-merge-two-sorted-lists/solution.ts
@@ -0,0 +1,20 @@
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummy = new ListNode(0)
+    let cur = dummy
+
+    while(list1 !== null && list2 !== null) {
+        if(list1.val <= list2.val) {
+            cur.next = list1
+            list1 = list1.next
+        } else {
+            cur.next = list2
+            list2 = list2.next
+        }
+
+        cur = cur.next
+    }
+
+    cur.next = list1 === null ? list2 : list1
+
+    return dummy.next
+};
