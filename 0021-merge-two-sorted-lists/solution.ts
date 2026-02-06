bde4bf187c63b54e7dc0050ddb92f88f573bc7f8
YuxianDu <yuxiandu5@gmail.com>
2026-02-06 04:46:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 58.9 MB (25.06%)

--- 0021-merge-two-sorted-lists/solution.ts
+++ 0021-merge-two-sorted-lists/solution.ts
@@ -1,3 +1,13 @@
+// return ListNode
+
+// 1. declare an empty ListNode to start with 
+// 2. declare a cur Node
+// 3. loop while list1 !== null || list2 !== null
+// 4. compare the next node for each list
+//     cur.next = smaller.next
+//     smaller = smaller.next
+// 5. return dummy.next
+
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     const dummy = new ListNode(0)
     let cur = dummy
@@ -10,7 +20,6 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
             cur.next = list2
             list2 = list2.next
         }
-
         cur = cur.next
     }
 
