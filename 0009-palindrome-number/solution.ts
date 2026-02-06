3d388c2796e39352a66350af4dc5da689ac6a497
YuxianDu <yuxiandu5@gmail.com>
2026-02-06 04:54:01 +0000

    [LeetCode Sync] Runtime - 5 ms (80.79%), Memory - 65.2 MB (64.14%)

--- 0009-palindrome-number/solution.ts
+++ 0009-palindrome-number/solution.ts
@@ -1,17 +1,15 @@
 // return boolean
 
-// 1. turn num into a string
-// 2. two pointers left and right
-// 3. while left < right 
-// 4. check if both are the same, if same incre and decre left and right each iteration
-// 5. after loop return true
+// 1. x to string
+// 2. declare left and right
+// 3. while(left < right)
+// 4. if x[left] !== x[right]
+//     return false
+// 5. return true
 
 function isPalindrome(x: number): boolean {
-    if(x < 0) {
-        return false
-    }
-
     const num = x.toString()
+
     let left = 0
     let right = num.length - 1
 
@@ -26,4 +24,3 @@ function isPalindrome(x: number): boolean {
 
     return true
 };
-
