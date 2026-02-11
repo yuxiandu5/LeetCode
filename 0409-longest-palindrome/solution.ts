df6b23b2db6983114ac8459494be709f29d0d285
YuxianDu <yuxiandu5@gmail.com>
2026-02-11 04:19:44 +0000

    [LeetCode Sync] Runtime - 3 ms (83.77%), Memory - 58.8 MB (11.59%)

--- 0409-longest-palindrome/solution.ts
+++ 0409-longest-palindrome/solution.ts
@@ -1,29 +1,21 @@
-// Output
-// 1. length which is a number
-
-// Approach
-// 1. first iteration record all letters and counts
-// 2. each count in record if even num add into length
-// 3. if odd - 1 and add length declare hasOdd = true plus 1 before returning result
+// 1. for each element add into a map element: count
+// 2. we add all counts if odd - 1 else count
+// 3. if cur < s.length plus 1
 
 function longestPalindrome(s: string): number {
     const record: Record<string, number> = {}
 
-    for(const letter of s) {
-        record[letter] = (record[letter] ?? 0) + 1
+    for(const char of s) {
+        record[char] = (record[char] ?? 0) + 1
     }
 
-    let length = 0
-    let hasOdd = false
+    let length: number = 0
 
-    for(const count of Object.values(record)) {
-        if(count % 2 === 0) {
-            length += count
-        } else {
-            length += (count - 1)
-            hasOdd = true
-        }
-    }
+    Object.values(record).forEach(count => {
+        length += count % 2 === 0 ? count : count - 1
+    })
+
+    const hasOdd = Object.values(record).some(count => count % 2 !== 0)
 
     return hasOdd ? length + 1 : length
- }; 
+};
