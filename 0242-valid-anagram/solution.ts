20c738805dc2024946370d9c9b44c23f3b6dc0ae
YuxianDu <yuxiandu5@gmail.com>
2026-02-16 03:18:12 +0000

    [LeetCode Sync] Runtime - 12 ms (68.26%), Memory - 61.7 MB (18.67%)

--- 0242-valid-anagram/solution.ts
+++ 0242-valid-anagram/solution.ts
@@ -1,7 +1,6 @@
-// 1. check if length are equal
-// 2. loop through s add each letter with count inao a hashmap
-// 3. loop through t if any of the value < 0 return false
-// 4. return true
+// 1. check for same length
+// 2. for loop record all letters in s
+// 3. for loop minus all count from record if value = 0 return false
 
 function isAnagram(s: string, t: string): boolean {
     if(s.length !== t.length) {
@@ -10,13 +9,17 @@ function isAnagram(s: string, t: string): boolean {
 
     const record: Record<string, number> = {}
 
-    for(let i = 0;i < s.length;i++) {
-        const charS = s[i]
-        const charT = t[i]
-
-        record[charS] = (record[charS] ?? 0) + 1
-        record[charT] = (record[charT] ?? 0) - 1
+    for(const char of s) {
+        record[char] = (record[char] ?? 0) + 1
     }
 
-    return Object.values(record).every(char => char === 0)
+    for(const char of t) {
+        if(!record[char]) {
+            return false
+        }
+
+        record[char] -= 1
+    }
+
+    return true
 };
