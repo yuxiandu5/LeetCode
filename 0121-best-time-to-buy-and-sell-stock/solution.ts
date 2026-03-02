051d4e486873f39821cbc748de6549a0aea1ba0f
YuxianDu <yuxiandu5@gmail.com>
2026-03-02 08:48:03 +0000

    [LeetCode Sync] Runtime - 6 ms (21.99%), Memory - 67.7 MB (8.98%)

--- 0121-best-time-to-buy-and-sell-stock/solution.ts
+++ 0121-best-time-to-buy-and-sell-stock/solution.ts
@@ -1,30 +1,11 @@
-// return number
-
-// 1. scan from left to right
-// 2. record the cheapest price to buy so far on the way, and declare profit
-// 3. compare the cheapest to the cur day price if cheaper than profit then profit = cur profit
-// 4. return profit > 0 ? profit : 0
-
 function maxProfit(prices: number[]): number {
-    if(prices.length === 1) {
-        return 0
-    }
-        
-    let cheapest = prices[0]
-    let profit = 0
+    let minBuy = prices[0]
+    let maxProfit = 0
 
-    for(let i = 1;i < prices.length;i++) {
-        const todayPrice = prices[i]
-        const todayProfit = todayPrice - cheapest
-
-        if(todayProfit > profit) {
-            profit = todayProfit
-        }
-
-        if(todayPrice < cheapest) {
-            cheapest = todayPrice
-        }
+    for(const cur of prices) {
+        minBuy = Math.min(cur, minBuy)
+        maxProfit = Math.max(maxProfit, cur - minBuy)
     }
 
-    return profit > 0 ? profit : 0
+    return maxProfit
 };
