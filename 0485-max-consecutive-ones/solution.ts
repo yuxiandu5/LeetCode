f60e3fbbe4c32d3a46484e976bdb997f1bc3529a
YuxianDu <yuxiandu5@gmail.com>
2026-03-03 06:39:52 +0000

    [LeetCode Sync] Runtime - 6 ms (9.30%), Memory - 62.3 MB (14.37%)

--- 0485-max-consecutive-ones/solution.ts
+++ 0485-max-consecutive-ones/solution.ts
@@ -1,22 +1,15 @@
-// return number
-
-// 1. declare max and cur
-// 2. if num === 1 cur ++
-//     if num === 0 max = max > cur ? max : cur
-//     cur === 0
-
 function findMaxConsecutiveOnes(nums: number[]): number {
-    let max = 0
-    let cur = 0
+    let result = 0
+    let count = 0
 
     for(const num of nums) {
         if(num === 1) {
-            cur++
+            count++
         } else {
-            max = max > cur ? max : cur
-            cur = 0
+            result = Math.max(count, result)
+            count = 0
         }
     }
 
-    return Math.max(max, cur)
+    return Math.max(result, count)
 };
