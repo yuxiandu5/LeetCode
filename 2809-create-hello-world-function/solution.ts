4f21172bb1b814d16bd6cfdc954e0379c9abaf41
YuxianDu <yuxiandu5@gmail.com>
2026-03-20 12:56:25 +0000

    [LeetCode Sync] Runtime - 30 ms (98.20%), Memory - 54.7 MB (69.39%)

---
+++ 2809-create-hello-world-function/solution.ts
@@ -0,0 +1,11 @@
+function createHelloWorld(...args) {
+    
+    return function(...args): string {
+        return "Hello World"
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
