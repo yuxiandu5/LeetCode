8ad725dcf1e37872f0413613bd299a763215dfc7
YuxianDu <yuxiandu5@gmail.com>
2026-03-24 05:44:53 +0000

    [LeetCode Sync] Runtime - 51 ms (28.98%), Memory - 54.3 MB (95.73%)

---
+++ 2761-array-reduce-transformation/solution.ts
@@ -0,0 +1,13 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    let sum = init
+
+    for(let i = 0;i < nums.length;i++) {
+        const value = fn(sum, nums[i])
+
+        sum = value
+    }
+
+    return sum
+};
