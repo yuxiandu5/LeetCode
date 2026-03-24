b7deee01eb2899c090929bf1c1a17b934e977669
YuxianDu <yuxiandu5@gmail.com>
2026-03-24 05:07:09 +0000

    [LeetCode Sync] Runtime - 47 ms (39.40%), Memory - 54.9 MB (72.98%)

---
+++ 2813-to-be-or-not-to-be/solution.ts
@@ -0,0 +1,22 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (num) => {
+            if(val === num) return true
+            else throw new Error("Not Equal")
+        },
+        notToBe: (num) => {
+            if(val !== num) return true
+            else throw new Error("Equal")
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
