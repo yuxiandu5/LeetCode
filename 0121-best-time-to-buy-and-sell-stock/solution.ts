a74b0aa8ee848c2b84124dff715d3a0826080ae7
YuxianDu <yuxiandu5@gmail.com>
2026-03-27 11:31:43 +0000

    [LeetCode Sync] Runtime - 3 ms (54.96%), Memory - 65.8 MB (61.18%)

--- 0121-best-time-to-buy-and-sell-stock/solution.ts
+++ 0121-best-time-to-buy-and-sell-stock/solution.ts
@@ -1,10 +1,17 @@
+// scan from left to right
+// cal profit = (today's price - minBuy) each iteration compare to maxProfit
+// record the min pirce as we iterate
+
 function maxProfit(prices: number[]): number {
     let minBuy = prices[0]
     let maxProfit = 0
 
-    for(const cur of prices) {
-        minBuy = Math.min(cur, minBuy)
-        maxProfit = Math.max(maxProfit, cur - minBuy)
+    for(let i = 1;i < prices.length;i++) {
+        const price = prices[i]
+        const profit = price - minBuy
+
+        maxProfit = Math.max(maxProfit, profit)
+        minBuy = Math.min(price, minBuy)
     }
 
     return maxProfit
