d52633be509aeccec6baee6d6424accce892b3b2
YuxianDu <yuxiandu5@gmail.com>
2026-04-10 12:01:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.8 MB (87.33%)

--- 0118-pascals-triangle/solution.ts
+++ 0118-pascals-triangle/solution.ts
@@ -1,26 +1,17 @@
-// return array
-
-// 1. create an result array with initially [[1]]
-// 2. for loop start with i = 1 when i < numRows
-// 3. fill a the cur row with i 1
-// 4. another for loop j = 1 j < i
-// 5. row[j] = prev[j - 1] + prev[j]
-// 5. push row
-// 6. reurn result
-
 function generate(numRows: number): number[][] {
-    const result = [[1]]
+    const results = [[1]]
 
     for(let i = 1; i < numRows; i++) {
-        const row = Array(i + 1).fill(1)
-        const prev = result[i - 1]
-        
-        for(let j = 1; j < i; j++) {
-            row[j] = prev[j - 1] + prev[j]
+        const prev = results[i - 1]
+        const newArr = [1]
+
+        for(let j = 1; j < prev.length; j++) {
+            newArr.push(prev[j - 1] + prev[j])
         }
 
-        result.push(row)
+        newArr.push(1)
+        results.push(newArr)
     }
 
-    return result
+    return results
 };
